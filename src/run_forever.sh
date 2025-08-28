e197db385aaca6e6fc995dac0a353935130d15c2
Nikolaj Gundstrup <gunniko@gmai.com>
2025-08-28 16:19:38 +0200

    Converted from python2 to 3

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 PYTHON_SCRIPT_PATH=$1
 
-TMP="This variable might become useful at some point. Otherwise delete it." 
-
-while true
-do
-    python2 $PYTHON_SCRIPT_PATH
-    if [ $? -ne 0 ]; then
-        echo "Script crashed with exit code $?. Restarting..." >&2
-        sleep 1
-    fi
+while true; do
+  python2 "$PYTHON_SCRIPT_PATH"
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "Script crashed with exit code $EXIT_CODE. Restarting..." >&2
+    sleep 1
+  fi
 done
+
