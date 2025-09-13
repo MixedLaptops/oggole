f6476c40a206699cdc6ad5adcc4f5740172ad1e2
Nikolaj Gundstrup <gunniko@gmai.com>
2025-09-13 20:20:06 +0200

    Used modern html on login

--- src/backend/app.go
+++ src/backend/app.go
@@ -22,7 +22,7 @@ func main() {
 		panic(err)
 	}
 	
-	templates = template.Must(template.ParseGlob("templates/*.html"))
+	templates = template.Must(template.ParseGlob("../templates/*.html"))
 
 	http.HandleFunc("/api/search", search)
 	http.HandleFunc("/api/login", login)
