19d639370b1b2ef5876d604d9c2d29587d7063f8
Nikolaj Gundstrup <gunniko@gmai.com>
2025-09-20 18:54:55 +0200

    Sync app.go with github development branch

--- src/backend/app.go
+++ src/backend/app.go
@@ -1,14 +1,19 @@
-package main	
+package main
 
 import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 
 	_ "modernc.org/sqlite"
 )
+
+// Sætter en general database variable op som kan aktiveres i main
+// og sørge for alt der skal tilgå den kan refere til den.
+var db *sql.DB
+var templates *template.Template
 
 type Page struct {
 	Title       string `json:"title"`
@@ -19,6 +24,15 @@ type Page struct {
 }
 
 func main() {
+	// Initialize database
+	var err error
+	db, err = sql.Open("sqlite", "whoknows.db")
+	if err != nil {
+		panic(err)
+	}
+
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+
 	http.HandleFunc("/api/search", search)
 	http.HandleFunc("/api/login", login)
 	http.HandleFunc("/api/register", register)
@@ -27,24 +41,25 @@ func main() {
 	http.HandleFunc("/login", login1)
 	http.HandleFunc("/weather", weather1)
 	http.HandleFunc("/register", register1)
+	http.HandleFunc("/about", about)
 	http.HandleFunc("/", index)
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func search(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("search"))	
-	return 
+	w.Write([]byte("search"))
+	return
 }
 
 func login(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "login")
-	return 
+	return
 }
 
 func register(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "login") 
-	return 
+	fmt.Fprint(w, "login")
+	return
 }
 
 func logout(w http.ResponseWriter, r *http.Request){
@@ -58,21 +73,22 @@ func weather(w http.ResponseWriter, r *http.Request){
 }
 
 func login1(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "login1")	
-	return
+	templates.ExecuteTemplate(w, "login.html", nil)
 }
 
 func weather1(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "weather1")	
+	fmt.Fprint(w, "weather1")
 	return
 }
 
 func register1(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "register1")	
-	return
+	templates.ExecuteTemplate(w, "register.html", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "index")	
-	return
+	templates.ExecuteTemplate(w, "search.html", nil)
+}
+
+func about(w http.ResponseWriter, r *http.Request){
+	templates.ExecuteTemplate(w, "about.html", nil)
 }
